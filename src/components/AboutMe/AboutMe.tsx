893417d0bea3e128e6510d5d8d3ab234062b8f6e
Nikan <nikankad@protonmail.com>
2023-11-25 12:24:49 -0500

    in the woroks

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { getAllByPlaceholderText } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 const AboutMe = () => {
-  return <div>AboutMe</div>;
+  const [aboutMeText, setAboutMeText] = useState("");
+
+  useEffect(() => {
+    const fetchAboutMe = async () => {
+      try {
+        const response = await fetch("public/textfiles/aboutme.txt");
+        console.log(response);
+        const text = await response.text();
+        setAboutMeText(text);
+      } catch (error) {
+        console.error("Error fetching about me text:", error);
+      }
+    };
+
+    fetchAboutMe();
+  }, []);
+
+  return (
+    <div>
+      <p>{aboutMeText}</p>
+    </div>
+  );
 };
 
 export default AboutMe;
